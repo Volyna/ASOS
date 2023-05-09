f293f40bd19fbe760083a932d9512e85f09e9425
Volyna <olegnovak30@gmail.com>
2023-05-09 18:45:22 +0300

    add neon.tech DB

---
+++ WebASOS/WebAsos/Data/Database.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace WebAsos.Data
+{
+    public interface Database
+    {
+        /*CREATE TABLE users AS SELECT now();
+        SELECT* FROM users;*/
+    }
+}
