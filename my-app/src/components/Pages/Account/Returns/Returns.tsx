05c094af53af9ce121885c36a4c1c4a76aa3c419
Маша Дердюк <mashaderduik@gmail.com>
2023-08-02 20:07:37 +0300

    add account_returns and account_returns_empty pages

--- my-app/src/components/Pages/Account/Returns/Returns.tsx
+++ my-app/src/components/Pages/Account/Returns/Returns.tsx
@@ -1,26 +1,25 @@
-import "./Payment.css";
 import Header_full from "../../../Header_full/Header_full";
 import Footer from "../../../Footer/FooterV";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const Payment = () => {
+const Returns = () => {
   return (
     <>
       <Header_full />
       <div className="container-fluid">
         <div className="mainLink">
           <p className="focusCom">focus.com | Account |</p>
-          <p className="focusCom basket"> Shopping history</p>
+          <p className="focusCom basket"> Returns</p>
         </div>
         <div className="container">
           <div className="menu_item">
             <Link className="account_item" to="/account/contact-information">
               Contact information
             </Link>
-            <Link className="main_item" to="/account/shopping-history-empty">
+            <Link className="account_item" to="/account/shopping-history-empty">
               shopping history
             </Link>
-            <Link className="account_item" to="/account/returns-empty">
+            <Link className="main_item" to="/account/returns-empty">
               returns
             </Link>
             <Link className="account_item" to="/">
@@ -35,79 +34,79 @@ const Payment = () => {
           <label className="col-2">Date</label>
           <label className="col-2">Amount</label>
           <label className="col-2">Order number</label>
-          <label className="col-2">Order status</label>
-          <label className="col-2">Parcel tracking number</label>
+          <label className="col-2">Total</label>
+          <label className="col-2">Read more information</label>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="user_payment">
           <p className="col-2">07.07.2023</p>
           <p className="col-2">123.00$</p>
           <p className="col-2">527297</p>
-          <p className="col-2">Payed</p>
+          <p className="col-2">123.00$</p>
           <a href="" className="link col-2">
-            Learn more
+            View details
           </a>
         </div>
         <div className="save show ">
@@ -119,4 +118,4 @@ const Payment = () => {
   );
 };
 
-export default Payment;
+export default Returns;
--- my-app/src/components/Pages/Account/ReturnsEmpty/ReturnsEmpty.tsx
+++ my-app/src/components/Pages/Account/ReturnsEmpty/ReturnsEmpty.tsx
@@ -1,26 +1,26 @@
+import "./ReturnsEmpty.css";
 import Header_full from "../../../Header_full/Header_full";
 import Footer from "../../../Footer/FooterV";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const PaymentEmpty = () => {
-  const navigate = useNavigate();
+const ReturnsEmpty = () => {
   return (
     <>
       <Header_full />
       <div className="container-fluid">
         <div className="mainLink">
           <p className="focusCom">focus.com | Account |</p>
-          <p className="focusCom basket"> Shopping history</p>
+          <p className="focusCom basket"> Returns</p>
         </div>
         <div className="container">
           <div className="menu_item">
             <Link className="account_item" to="/account/contact-information">
               Contact information
             </Link>
-            <Link className="main_item" to="/account/shopping-history-empty">
+            <Link className="account_item" to="/account/shopping-history-empty">
               shopping history
             </Link>
-            <Link className="account_item" to="/account/returns-empty">
+            <Link className="main_item" to="/account/returns-empty">
               returns
             </Link>
             <Link className="account_item" to="/">
@@ -35,15 +35,14 @@ const PaymentEmpty = () => {
           <label className="col-2">Date</label>
           <label className="col-2">Amount</label>
           <label className="col-2">Order number</label>
-          <label className="col-2">Order status</label>
-          <label className="col-2">Parcel tracking number</label>
+          <label className="col-2">Total</label>
+          <label className="col-2">Read more information</label>
         </div>
 
         <div className="content col-12">
-          <p className="basket_text">Ops, you haven’t made any orders yet!</p>
-          <button onClick={() => navigate("/")} className="btn_continue">
-            continue shopping
-          </button>
+          <p className="basket_text returns">
+            Great news, you haven’t asked for returns yet!
+          </p>
         </div>
       </div>
       <Footer />
@@ -51,4 +50,4 @@ const PaymentEmpty = () => {
   );
 };
 
-export default PaymentEmpty;
+export default ReturnsEmpty;
