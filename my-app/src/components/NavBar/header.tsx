aa51fa468e895d7c4496230dae36cebc56e040f2
danylo454 <ilchuk.danilo@gmail.com>
2023-09-11 20:40:32 +0300

    fix navbar

--- my-app/src/components/NavBar/header.tsx
+++ my-app/src/components/NavBar/header.tsx
@@ -52,8 +52,7 @@ const Header = () => {
 
           <div className="userIcons">
             {isAuth == true ? (
-              <Link to="/account/contact-information">
-
+              <Link className="noneM" to="/account/contact-information">
                 <img src={selectedUser} alt="" />
               </Link>
             ) : (
@@ -62,10 +61,9 @@ const Header = () => {
               </Link>
             )}
 
-            <Link to="/basket"/>
-              <img src={cart} alt="basket" className="noneM basket" />
+            <Link to="/basket" />
+            <img src={cart} alt="basket" className="noneM basket" />
 
-               
             <Link to="/account/favourites">
               <img src={like} className="favourites" alt="favourites" />
             </Link>
